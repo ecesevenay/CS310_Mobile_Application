559d806ba002828a38990e1f140c19336b60a4b2
duygubuket <94993214+duygubuket@users.noreply.github.com>
2021-11-30 17:12:40 +0300

    Add files via upload

--- lib/pages/signin.dart
+++ lib/pages/signin.dart
@@ -1,25 +1,145 @@
-import 'package:cs_310_proje/utils/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:cs_310_proje/utils/color.dart';
 
-class SignUp extends StatefulWidget {
-  @override
-  _SignUpState createState() => _SignUpState();
-}
-
-class _SignUpState extends State<SignUp> {
+class SignupPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
+      //resizeToAvoidBottomPadding: false,
+      backgroundColor : AppColors.maincolor,
       appBar: AppBar(
-        title: Text(
-          'SIGNUP',
-          style: kAppBarTitleTextStyle,
-        ),
+        elevation: 0,
+        brightness: Brightness.light,
         backgroundColor: AppColors.maincolor,
-        centerTitle: true,
-        elevation: 0.0,
+        leading:
+        IconButton( onPressed: (){
+          Navigator.pop(context);
+        },icon:Icon(Icons.arrow_back_ios,size: 20,color: Colors.black,)),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            width: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text ("Sign up!", style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold, color: AppColors.backgroundColor
+                        ),),
+                        SizedBox(height: 20,),
+                        Text("Create an Account",style: TextStyle(
+                          fontSize: 15,
+                          fontWeight:FontWeight.bold,
+                          color: AppColors.backgroundColor.withOpacity(0.5),
+                        ),),
+                        SizedBox(height: 30,)
+                      ],
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: 40
+                      ),
+                      child: Column(
+                        children: [
+                          makeInput(label: "Email"),
+                          makeInput(label: "Password",obsureText: true),
+                          makeInput(label: "Confirm Password",obsureText: true),
+                          makeInput(label: "Are you a student or a graduate? (G/S) (G for graduate and S for Student)",obsureText: false),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 40),
+                      child: Container(
+                        padding: EdgeInsets.only(top: 3,left: 3),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(40),
+                            border: Border(
+                                bottom: BorderSide(color: Colors.white),
+                                top: BorderSide(color: Colors.white),
+                                right: BorderSide(color: Colors.white),
+                                left: BorderSide(color: Colors.white)
+                            )
+                        ),
+                        child: MaterialButton(
+                          minWidth: double.infinity,
+                          height:60,
+                          onPressed: (){},
+                          color: Colors.white70,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(40)
+                          ),
+                          child: Text("Sign Up",style: TextStyle(
+                            fontWeight: FontWeight.w800,fontSize: 16, color:Colors.black
+
+                          ),),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Already have an account? ", style: TextStyle(
+                            color: AppColors.backgroundColor.withOpacity(0.5),
+                            fontWeight: FontWeight.w600)
+
+                        ),
+                        Text("Login",style: TextStyle(
+                            color: AppColors.backgroundColor.withOpacity(0.5),
+                            fontWeight: FontWeight.w600,
+                            fontSize: 18
+                        ),),
+                      ],
+                    )
+                  ],
+
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
+}
+
+Widget makeInput({label, obsureText=false}){
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Text(label,style:TextStyle(
+          fontSize: 15,
+          fontWeight: FontWeight.bold,
+          color: AppColors.backgroundColor
+      ),),
+      SizedBox( height: 5),
+      TextField(
+        obscureText: obsureText,
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: Colors.white70,
+          contentPadding: EdgeInsets.symmetric(vertical: 0,horizontal: 10),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.white,
+            ),
+          ),
+          border: OutlineInputBorder(
+              borderSide: BorderSide(color: AppColors.backgroundColor)
+          ),
+        ),
+      ),
+      SizedBox(height: 30,)
+
+    ],
+  );
 }
