5744ae971b4c8b46451ce1a4cbd99651041e2b1c
Ece Sevenay <72407952+ecesevenay@users.noreply.github.com>
2021-11-30 17:32:42 +0300

    styles file updated

--- utils/styles.dart
+++ utils/styles.dart
@@ -1,26 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:cs_310_proje/utils/color.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-final kHeadingTextStyle = TextStyle(
+final kHeadingTextStyle = GoogleFonts.montserrat(
   color: AppColors.maincolor,
   fontWeight: FontWeight.w500,
-  fontSize: 30.0,
+  fontSize: 40.0,
   letterSpacing: -0.7,
 );
 
-final kButtonLightTextStyle = TextStyle(
+final kButtonLightTextStyle = GoogleFonts.lato(
   color: AppColors.primary,
-  fontSize: 20.0,
+  fontSize: 25.0,
   letterSpacing: -0.7,
 );
 
-final kButtonDarkTextStyle = TextStyle(
+final kButtonDarkTextStyle = GoogleFonts.oswald(
   color: AppColors.primaryDark,
-  fontSize: 20.0,
+  fontSize: 25.0,
   letterSpacing: -0.7,
 );
 
-final kAppBarTitleTextStyle = TextStyle(
+final kAppBarTitleTextStyle = GoogleFonts.raleway(
   color: Colors.white,
   fontSize: 24.0,
   fontWeight: FontWeight.w600,
